b506ea2048155cea4800215271fce49218ff9626
sagardas25 <sagar.xyz05@gmail.com>
2025-02-01 16:04:20 +0530

    new projects

---
+++ 06-Todo-Context/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo msg",
+      completed: false,
+    },
+  ],
+
+  addTodo: (todo) => {},
+  updateTodo: (todo, id) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoContextProvider = TodoContext.Provider;
